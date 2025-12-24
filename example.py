8d1d81e654952d89f5b4812dbf32713c2e594999
Erik Mojica <149959329+EKMoca@users.noreply.github.com>
2025-12-24 16:21:35 +0000

    upload example file

--- example.py
+++ example.py
@@ -1,1 +1,8 @@
-print(False - True)
+foo = 42            # foo is 42
+foo -= 2            # foo is now 40
+foo *= 3            # foo is now 120
+foo += 5            # foo is now 125
+foo //= 25          # foo is now 5
+foo /= 2            # foo is now 2.5
+foo **= 3           # foo is now 15.625
+print(foo)          # prints 15.625
